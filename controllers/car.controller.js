cde98d9fc2ab17231bf2c61fceeede247d413fa1
Hong Phuc Nguyen <118801428+hongpnguyen13198@users.noreply.github.com>
2023-04-15 18:27:30 +0700

    updating one more time

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -3,8 +3,7 @@ const Car = require('../models/Car');
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	const { make, model, transmission_type, size, style, release_date, price } =
-    req.body;
+  const { make, model, transmission_type, size, style, release_date, price } = req.body;
   const info = {
     make,
     model,
@@ -14,104 +13,83 @@ carController.createCar = async (req, res, next) => {
     release_date,
     price,
   };
-	try {
-		// YOUR CODE HERE
-		if(!info) throw new AppError(402,"Bad Request","Create car Error")
-		const created= await Foo.create(info)
-		sendResponse(res,200,true,{data:created},null,"Create car Success")
-	} catch (err) {
-		// YOUR CODE HERE
-		next(err)
-	}
+  try {
+    if (!info) {
+      throw new AppError(402, "Bad Request", "Create car Error");
+    }
+    const created = await Foo.create(info);
+    sendResponse(res, 200, true, { data: created }, null, "Create car Success");
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.getCars = async (req, res, next) => {
-	const filter = {}
-	try {
-		// YOUR CODE HERE
-		const filter = { isDeleted: { $eq: false } };
-
-let { page } = req.query;
-
-page = parseInt(page) || 1;
-
-const limit = 10;
-
-let offset = limit * (page - 1);
-
-const listOfCar = await Car.find(filter).sort({ createdAt: -1 });
-
-const result = listOfCar.slice(offset, offset + limit);
-
-const total = listOfCar.length;
-
-res.status(200).send({
-  cars: result,
-  message: "Get Car List Successfully!",
-  page: page,
-  total: total,
-});
-
-		
-	} catch (err) {
-		// YOUR CODE HERE
-		next(err)
-	}
+  const filter = { isDeleted: { $eq: false } };
+  let { page } = req.query;
+  page = parseInt(page) || 1;
+  const limit = 10;
+  let offset = limit * (page - 1);
+  try {
+    const listOfCar = await Car.find(filter).sort({ createdAt: -1 });
+    const result = listOfCar.slice(offset, offset + limit);
+    const total = listOfCar.length;
+    res.status(200).send({
+      cars: result,
+      message: "Get Car List Successfully!",
+      page: page,
+      total: total,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.editCar = async (req, res, next) => {
-	const { id } = req.params;
-const { make, model, transmission_type, size, style, release_date, price } = req.body;
-const updateInfo = {
-  make,
-  model,
-  transmission_type,
-  size,
-  style,
-  release_date,
-  price,
-};
-const targetId = id;
-const options = { new: true };
-
-if (Object.keys(updateInfo).length === 0) {
-  throw new Error("field is invalid");
-}
-
-	try {
-		// YOUR CODE HERE
-		const updated = await Car.findByIdAndUpdate(targetId, updateInfo, options);
+  const { id } = req.params;
+  const { make, model, transmission_type, size, style, release_date, price } = req.body;
+  const updateInfo = {
+    make,
+    model,
+    transmission_type,
+    size,
+    style,
+    release_date,
+    price,
+  };
+  const targetId = id;
+  const options = { new: true };
+  try {
+    if (Object.keys(updateInfo).length === 0) {
+      throw new Error("field is invalid");
+    }
+    const updated = await Car.findByIdAndUpdate(targetId, updateInfo, options);
     res.status(200).send({ message: "Edit car successfully", car: updated });
-	} catch (err) {
-		// YOUR CODE HERE
-		next(err);
-	}
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	const { id } = req.params;
+  const { id } = req.params;
   const targetId = id;
   const options = { new: true };
-	try {
-		// YOUR CODE HERE
-		const deletedCar = await Car.findByIdAndUpdate(
-			targetId,
-			{
-			  isDeleted: true,
-			},
-			options
-		  );
-		  if (!deletedCar) throw new Error("Car is not exist");
-	  
-		  deletedCar = await deletedCar.save();
-		  res
-      .status(200)
-      .send({ cars: deletedCar, message: "Delete car successfully" });
-		
-	} catch (err) {
-		// YOUR CODE HERE
-		next(err)
-	}
+  try {
+    const deletedCar = await Car.findByIdAndUpdate(
+      targetId,
+      {
+        isDeleted: true,
+      },
+      options
+    );
+    if (!deletedCar) {
+      throw new Error("Car does not exist");
+    }
+    deletedCar = await deletedCar.save();
+    res.status(200).send({ cars: deletedCar, message: "Delete car successfully" });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = carController;
